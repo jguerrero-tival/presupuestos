33f99733486dc67d03d24b0ffe9cb3a094f87ea0
slavie <sebastianlavie@gmail.com>
2013-09-28 00:12:18 -0300

    Fix User class returned by service interface

--- sources/presupuestar/presupuestar-user/src/main/java/com/presupuestar/user/service/CommerceService.java
+++ sources/presupuestar/presupuestar-user/src/main/java/com/presupuestar/user/service/CommerceService.java
@@ -1,13 +1,12 @@
 package com.presupuestar.user.service;
 
 import com.presupuestar.model.user.Commerce;
-import com.presupuestar.model.user.User;
 
 public interface CommerceService {
 
 	Commerce registerNewCommerceAccount(String email, String password,
 			String reason);
 
-	User userLogin(String email, String password);
+	Commerce userLogin(String email, String password);
 
 }
